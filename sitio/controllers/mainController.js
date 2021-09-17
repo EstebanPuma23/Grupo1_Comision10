e486cff1d4c2736ca5acaf5749e0d23450e7b2ce
ErnestoC10 <sanchezernestoalejandro@gmail.com>
2021-09-17 09:28:46 -0300

    admin done

--- sitio/controllers/mainController.js
+++ sitio/controllers/mainController.js
@@ -1,4 +1,6 @@
 const products = require('../data/products.json');
+const fs = require ('fs');
+const path = require ('path')
 
 module.exports = {
     index : (req,res) => {
@@ -7,6 +9,12 @@ module.exports = {
     store : (req,res) => {
         return res.render('store', { 
             products
+        })
+    },
+    admin : (req,res) => {
+        return res.render('admin',{
+            title : "Administración",
+            products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
         })
     }
 }
--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -1,9 +1,46 @@
-const products = require('../data/products.json');
+const fs = require('fs');
+const path = require('path');
+const  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
 
 module.exports = {
     detail : (req,res) => {
         return res.render('product-view', {
             product : products.find(product => product.id === +req.params.id)
         })
+    },
+    add : (req,res) => {
+        return res.render('productAdd')
+    },
+    store : (req,res) => {
+        const {name,description,price} = req.body;
+        let product = {
+            id : products[products.length - 1].id + 1,
+            name : name.trim(),
+            description : description.trim(),
+            price : +price,
+        }
+        products.push(product);
+        fs.writeFileSync(path.join(__dirname,'..','data','products.json'),JSON.stringify(products,null,3),'utf-8');
+        return res.redirect('/admin')
+    },
+    edit : (req,res) => {
+        return res.render('productEdit',{
+            product : products.find(product => product.id === +req.params.id),
+        })
+    },
+    update : (req,res) => {
+        /* return res.send ('Ruta Correcta') */
+        const {name,description,price} = req.body;
+        let product = products.find(product => product.id === +req.params.id);
+        let productModified = {
+            id : +req.params.id,
+            name : name.trim(),
+            description : description.trim(),
+            price : +price,
+        }
+
+        let productsModified = products.map(product => product.id === +req.params.id ? productModified : product);
+        fs.writeFileSync(path.join(__dirname,'..','data','products.json'),JSON.stringify(productsModified,null,3),'utf-8');
+        return res.redirect('/admin')
     }
 }

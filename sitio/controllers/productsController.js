26045e2a7debcc8b47b872c3310542001f99c578
Franco-Lopez <f.dlopez_97@hotmail.com>
2021-09-17 11:02:46 -0300

    sube descripcion de los productos

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -3,7 +3,7 @@ const products = require('../data/products.json');
 module.exports = {
     detail : (req,res) => {
         return res.render('product-view', {
-            product : products.find(product => product.id === +req.params.id)
+            product : products.find(product => product.id === +req.params.id),
         })
     }
 }

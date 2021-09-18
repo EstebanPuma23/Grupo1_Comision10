cdac31e3ff625298b04b07d834e4386b7cb344cb
Esteban <puma.esteban18@gmail.com>
2021-09-18 17:02:43 -0300

    app.listen agregado a app.js

--- sitio/app.js
+++ sitio/app.js
@@ -28,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
+
+app.listen(console.log(`Servidor corriendo en http://localhost:3000`))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

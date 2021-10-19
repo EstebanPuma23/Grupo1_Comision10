1037651cebf524d10580c12794f9bc28278049a7
Esteban <puma.esteban18@gmail.com>
2021-10-18 22:39:32 -0300

    agrego validaciones de administradores

--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const adminUserCheck = require('../middlewares/adminUserCheck')
 
 const {detail, add, store, edit, update, destroy} = require('../controllers/productsController');
 
 
 router.get('/detail/:id', detail);
-router.get('/add',add);
+router.get('/add',adminUserCheck,add);
 router.post('/add',store);
-router.get('/edit/:id',edit);
+router.get('/edit/:id',adminUserCheck,edit);
 router.put('/update/:id',update)
 router.delete('/destroy/:id', destroy)
 

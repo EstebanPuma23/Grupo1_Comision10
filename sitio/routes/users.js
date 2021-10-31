4f8d7fc202650128d8999595a589db3996f0dac1
Esteban <puma.esteban18@gmail.com>
2021-10-31 14:32:53 -0300

    agrego restricciones de usuario en login y register

--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const loginValidator = require('../validations/loginValidator');
-
+const loginUsercheck = require('../middlewares/loginUserCheck')
 const {register, processRegister, login, processLogin} = require('../controllers/usersController');
 
 /* GET home page. */
-router.get('/register', register)
+router.get('/register',loginUsercheck, register)
 router.post('/register',processRegister)
-router.get('/login',login)
+router.get('/login',loginUsercheck, login)
 router.post('/login',loginValidator, processLogin)
 
 module.exports = router;

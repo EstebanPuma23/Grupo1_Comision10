33d017910abd2aa2884a5529988093dd2bdbf9ec
Franco-Lopez <f.dlopez_97@hotmail.com>
2021-11-01 10:41:13 -0300

    sube validaciones registro

--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const loginValidator = require('../validations/loginValidator');
+const registerValidator = require('../validations/registerValidator');
+
 const loginUsercheck = require('../middlewares/loginUserCheck')
 const {register, processRegister, login, processLogin} = require('../controllers/usersController');
 
 /* GET home page. */
 router.get('/register',loginUsercheck, register)
-router.post('/register',processRegister)
+router.post('/register',registerValidator ,processRegister)
 router.get('/login',loginUsercheck, login)
 router.post('/login',loginValidator, processLogin)
 

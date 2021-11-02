7a7cf36f0addb4c99d512c54e37db998feb6fb3d
Esteban Puma <puma.esteban18@gmail.com>
2021-11-02 02:48:16 -0300

    rutas actualizadas por cambio de estructura

--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -7,7 +7,6 @@ const loginUsercheck = require('../middlewares/loginUserCheck')
 const {register, processRegister, login, processLogin, logout} = require('../controllers/usersController');
 
 /* GET home page. */
-<<<<<<< HEAD:sitio/routes/users.js
 router
     .get('/register',loginUsercheck, register)
     .post('/register',processRegister)
@@ -15,11 +14,5 @@ router
     .post('/login',loginValidator, processLogin)
     .get('/logout',logout)
 
-=======
-router.get('/register',loginUsercheck, register)
-router.post('/register',registerValidator ,processRegister)
-router.get('/login',loginUsercheck, login)
-router.post('/login',loginValidator, processLogin)
->>>>>>> fcd45407fe90f2a5c6a58f072a3f8feb4139e83f:sitio/src/routes/users.js
 
 module.exports = router;

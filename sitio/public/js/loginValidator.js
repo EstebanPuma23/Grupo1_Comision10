8591f421b34aea0f0bb22cdbfcd22a2b5d225cdb
Esteban Puma <puma.esteban18@gmail.com>
2021-12-04 02:47:23 -0300

    validaciones front del Login terminadas

---
+++ sitio/public/js/loginValidator.js
@@ -0,0 +1,65 @@
+const $ = id => document.getElementById(id);
+
+const inputEmail = $('email');
+const inputPassword = $('pass');
+const button = $('button')
+
+const infoEmail = $('info-email')
+const infoPass = $('info-password')
+
+const errorEmail = $('error-email')
+const errorPass = $('error-password')
+
+const regExEmail = /^(([^<>()\[\]\,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/
+    /* const regExLetras = /^[_A-zA-Z]*((-|\s)*[_A-zA-Z])*$/ */
+    /* const regExPassword = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/ */
+
+/* Validation Email */
+
+
+inputEmail.addEventListener('focus', function() {
+    infoEmail.innerText = 'Solo se permiten letras'
+    this.classList.remove('is-invalid')
+})
+
+inputEmail.addEventListener('keydown', function() {
+    infoEmail.innerText = null
+    errorEmail.innerText = null
+})
+
+inputEmail.addEventListener('blur', function() {
+    switch (true) {
+        case !this.value:
+            errorEmail.innerText = 'El email es obligatorio';
+            this.classList.add('is-invalid')
+            break;
+        case !regExEmail.test(this.value):
+            errorEmail.innerText = '¡Email invalido!';
+            this.classList.add('is-invalid');
+            break;
+        default:
+            errorEmail.innerText = null
+            this.classList.remove('is-invalid')
+            this.classList.add('is-valid')
+            break;
+    }
+})
+
+
+/* Validation password */
+
+inputPassword.addEventListener('blur', function() {
+    if (!this.value) {
+        errorPass.innerText = 'La contraseña es obligatoria'
+        this.classList.add('is-invalid')
+    }
+})
+
+/* button block */
+
+button.addEventListener('click', function(e) {
+    if ((errorPass || errorEmail) != '') {
+        $('error-button').innerText = 'Ingresa tus datos'
+        e.preventDefault()
+    }
+})
--- sitio/src/validations/loginValidator.js
+++ sitio/src/validations/loginValidator.js
@@ -1,16 +1,15 @@
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 const users = require('../data/users.json');
 const bcrypt = require('bcryptjs');
 
-module.exports =  [
+module.exports = [
     body('email')
-        .custom((value, {req}) =>{
-            let user = users.find(user => user.email === value && bcrypt.compareSync(req.body.password, user.password));
-            if(user){
-                return true;
-            }else{
-                return false;
-            }
-        }).withMessage('Credenciales invalidas')
+    .custom((value, { req }) => {
+        let user = users.find(user => user.email === value && bcrypt.compareSync(req.body.password, user.password));
+        if (user) {
+            return true;
+        } else {
+            return false;
+        }
+    }).withMessage('Email o contraseña incorrectos')
 ]
-    

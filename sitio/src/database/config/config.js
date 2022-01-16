a7ede965da76055d70b86a8039a8dc9ac49f87ca
Esteban Puma <puma.esteban18@gmail.com>
2022-01-16 17:46:48 -0300

    update config.js

--- sitio/src/database/config/config.js
+++ sitio/src/database/config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "rood",
-    "database": "innovarte",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "3306"
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port : process.env.DB_PORT
   },
   "test": {
     "username": "root",
